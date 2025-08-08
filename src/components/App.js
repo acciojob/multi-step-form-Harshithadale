f8bf407f7f95bcc051a9df73ca936b26fdf89e09
harshithadale <daleharshi6045@gmail.com>
2025-08-08 13:14:48 +0530

    initial-commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Step from "./Step";
 
 const App = () => {
+  const [ind,setInd] = useState(1)
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Step ind={ind} setInd={setInd}/>
     </div>
   )
 }
---
+++ src/components/Step.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+function Step({ind,setInd}) {
+  return (
+    <form>
+        {
+            ind === 1 ? (<div>
+                <>
+                    <h1>Customer Details</h1>
+                    <label>First Name</label>
+                    <input type="text" id="first_name"/>
+                    <br/>
+                    <label>Last Name</label>
+                    <input type="text" id="last_name"/>
+                    <br/>
+                    <button onClick={()=>setInd(prev=>prev+1)}>Next</button>
+                </>
+            </div>):""
+        }
+        {
+            ind == 2 ? (<div>
+                <h1>Car Details</h1>
+                <label>Price:</label>
+                <input id="car_price"/>
+                <br/>
+                <label>Model:</label>
+                <input id="model"/>
+                <br/>
+                <button onClick={()=>setInd(prev=>prev-1)}>Previous</button>
+                <button onClick={()=>setInd(prev=>prev+1)}>Next</button>
+            </div>):""
+        }
+        {
+            ind === 3 ? (<div>
+                <h1>Payment Details</h1>
+                <label>Credit card Number:</label>
+                <input id="card_info" type="number"/>
+                <br/>
+                <label>Expiry Date:</label>
+                <input type='date' id="expiry_date"/>
+                <br/>
+                <button onClick={()=>setInd(prev=>prev+1)}>Previous</button>
+                <button onClick={()=>setInd(prev=>1)}>Submit</button>
+            </div>):""
+        }
+    </form>
+  )
+}
+
+export default Step
